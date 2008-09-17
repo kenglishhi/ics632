d81196e282f2012ac206425b8603bc62369b0069
Kevin English <kenglish_hi@yahoo.com>
2008-09-16 22:41:34 -1000

    new files for my homework

--- hw1/exercise_1/question_2/matrix_multiply_with_blocks.c
+++ hw1/exercise_1/question_2/matrix_multiply_with_blocks.c
@@ -7,6 +7,7 @@
 static float getTimeDiff(struct timeval *s, struct timeval *e);
 int get_block_begin(int block_size, int offset) ;
 int get_block_end(int block_size, int offset) ;
+int multiply_matrices(double *result, double *a, double *b, int matrix_size) ;
 
 static float getTimeDiff(struct timeval *s, struct timeval *e) {
     struct timeval diff_tv;
@@ -21,6 +22,55 @@ static float getTimeDiff(struct timeval *s, struct timeval *e) {
 
     return (float) diff_tv.tv_sec + ((float) diff_tv.tv_usec / 1000000.0);
 }
+
+void print_matrices(double *result, double *a, double *b, int matrix_size) {
+    double *current_result;
+    double *current_a;
+    double *current_b;
+    int i,j;
+
+    printf("matrix_size = %d \n\n", matrix_size  );
+    for (i = 0; i < matrix_size; i++) {
+	printf(" [" );
+	for (j = 0; j < matrix_size; j++){
+	    current_a = a + i*matrix_size + j;
+	    printf(" %4.1f", *current_a  );
+	}
+	printf("] " );
+	printf("[" );
+	for (j = 0; j < matrix_size; j++){
+	    current_b = b + i*matrix_size + j;
+	    printf(" %4.1f ", *current_b  );
+	}
+	printf("] = " );
+	printf("[ " );
+	for (j = 0; j < matrix_size; j++){
+	    current_result = result + i * matrix_size + j;
+	    printf(" %4.1f ", *current_result  );
+	}
+	printf("] " );
+
+	printf("\n" );
+    }
+}
+
+void test_multiply_matrices() {
+    int matrix_size = 3;
+    double matrix1[matrix_size][matrix_size];
+    double matrix2[matrix_size][matrix_size];
+    double result[matrix_size][matrix_size];
+    int i, j;
+    for (i = 0; i < matrix_size; i++)  {
+	for (j = 0; j < matrix_size; j++){
+	    matrix1[i][j] = (i+1.0);
+	    matrix2[i][j] =  (i +1.0);
+	    result[i][j] = 0.0;
+	}
+    }
+    multiply_matrices(&result[0][0], &matrix1[0][0], &matrix2[0][0], matrix_size ) ;
+    print_matrices(&result[0][0], &matrix1[0][0], &matrix2[0][0], matrix_size ) ;
+}
+
 int get_block_begin(int block_size, int offset) {
     return (offset * block_size) ;
 }
@@ -28,6 +78,7 @@ int get_block_begin(int block_size, int offset) {
 int get_block_end(int block_size, int offset) {
     return (offset + 1 ) * block_size - 1;
 }
+
 int multiply_matrices(double *result, double *a, double *b, int matrix_size) {
     double *current_result;
     double *current_a;
@@ -35,83 +86,111 @@ int multiply_matrices(double *result, double *a, double *b, int matrix_size) {
     int i,j,k;
     for (i = 0; i < matrix_size; i++) {
 	for (k = 0; k < matrix_size; k++) {
+	    current_a = a + i*matrix_size + k;
 	    for (j = 0; j < matrix_size; j++) {
 		current_result = result + i*matrix_size + j;
-		current_a = a + i*matrix_size + k;
 		current_b = b + k*matrix_size + j;
 		*current_result += *current_a *  *current_b ;
 	    }
 	}
     }
+    return 1;
 }
+
+int calculate_block(double *result, double *a, double *b, int matrix_size, int block_size, int number_of_blocks, int row, int col) {
+    int i, j, k, x  ;
+    double *current_result ;
+    int a_location, b_location;
+    double *current_a ;
+    double *current_b ;
+
+
+    for (i=0 ; i < block_size; i++ ) {
+	for (j=0 ; j < block_size; j++ ) {
+	    current_result = result + (row * block_size* matrix_size + i*matrix_size) + (col*block_size + j)    ;
+	    for (x= 0 ; x < number_of_blocks; x++) {
+		for (k=0; k < block_size; k++ ) {
+		    a_location = (row * block_size* matrix_size + i*matrix_size) + k + x * block_size;
+		    b_location = (col*block_size + j) + k * matrix_size + x*block_size*matrix_size   ;
+		    current_a = a + a_location;
+		    current_b = b + b_location ;
+
+//          printf("current location %d  \n",  (row * block_size* matrix_size + i*matrix_size) + (col*block_size + j) ) ;
+//          printf("-- location %d * location %d   \n", a_location, b_location ) ;
+		    *current_result += (double)(*current_a) * (double)(*current_b);
+		}
+	    }
+	}
+    }
+    return 1;
+}
+
 //int multiple_matrix(int *result[], int *)
 int main(int argc,char *argv[])
 {
 
     if (argc < 3) {
-	printf("Must supply option for Matrix Size and block size, eg: matrix_multiply.exe 400 32 \n");
-	return -1;
+	printf("Must supply option for Matrix Size and block size, eg: matrix_multiply.exe 400 32 \n"); return -1;
     }
     int debug = 0;
     if (argc == 4) {
 	debug = 1;
     }
+
     int matrix_size = atoi(argv[1]);
-    printf("Matrix_size is  %d\n", matrix_size);
-
     int block_size = atoi(argv[2]);
-    printf("Block Size %d\n", matrix_size);
+    int number_of_blocks = matrix_size / block_size;
+//    test_multiply_matrices();
 
+
+    printf("Matrix_size is  %d\n", matrix_size);
+    printf("Block Size %d\n", matrix_size);
+//    matrix_size = 4;
+//    block_size = 2;
+//    number_of_blocks = 2;
     double matrix1[matrix_size][matrix_size];
     double matrix2[matrix_size][matrix_size];
     double result[matrix_size][matrix_size];
 
     struct timeval start;
     struct timeval end;
-    double time_elapsed;
-    int i,k,j;
+//    double time_elapsed;
+    int i,j;
 
     int seed = 10000;
     srand(seed);
 
     // POPULATE the array
+
     for (i = 0; i < matrix_size; i++)  {
 	for (j = 0; j < matrix_size; j++){
-	    matrix1[i][j] = (double) rand();
-	    matrix2[i][j] = (double) rand();
+	    matrix1[i][j] = 0.0 + (double)(( i * matrix_size) + j)  ;
+	    matrix2[i][j] = 0.0 + (double)(( i * matrix_size) + j)  ;
+	    if (debug) {
+		printf("matrix2[%d][%d] = %6.1f, -- ", i,j,matrix2[i][j] ) ;
+		printf("value = %6.1f,\n",  1.0 + (double)(( i * matrix_size) + j) ) ;
+		printf("\n");
+	    }
 	    result[i][j] = 0.0;
 	}
     }
+    printf("\n \n" ) ;
+//    multiply_matrices(&result[0][0], &matrix1[0][0], &matrix2[0][0], matrix_size ) ;
 
-
-    /*
-     * #2 : Do the MATRIX Multiplication i-k-j
-     */
+//    print_matrices(&result[0][0],&matrix1[0][0], &matrix2[0][0],matrix_size);
 
     gettimeofday(&start,NULL);
-    int number_of_blocks = matrix_size/block_size;
-
-    printf("Number of Blocks: %d\n", number_of_blocks);
-
-    printf("\nHere we go \n");
-    char *implementation = "i-k-j";
-    int inner_i, inner_j, inner_k;
-    int a,b,c;
-    int block_begin_a, block_end_a;
-    int block_begin_b, block_end_b;
-    int block_begin_c, block_end_c;
-    int total_multiplications = 0;
-
-    for (a=0; a < number_of_blocks; a++ ) {
-	for (b=0; b < number_of_blocks; b++ ) {
-	    for (c=0; c < number_of_blocks; c++ ) {
-		multiply_matrices(&result[a][0], &matrix1[b][0], &matrix2[c][0], block_size) ;
-	    }
+    for (i = 0; i < number_of_blocks; i++)  {
+	for (j = 0; j < number_of_blocks; j++){
+	    calculate_block(&result[0][0],&matrix1[0][0], &matrix2[0][0],matrix_size,block_size,number_of_blocks, i, j);
 	}
     }
-    gettimeofday(&end,NULL);
-    printf("Time difference for %s implementation %.5f seconds\n", implementation, getTimeDiff(&start, &end));
-    printf("Total Multiplications : %d\n", total_multiplications);
 
+//    calculate_block(&result[0][0],&matrix1[0][0], &matrix2[0][0],matrix_size,block_size,number_of_blocks, 1, 1);
+    if (debug) {
+	print_matrices(&result[0][0],&matrix1[0][0], &matrix2[0][0],matrix_size);
+    }
+    gettimeofday(&end,NULL);
+    printf("Time difference for block implementation  %.5f seconds\n",  getTimeDiff(&start, &end));
     return 1;
 }
