ae628a6a8d1e3823b944c2c20ad2eec27bca7ca4
Kevin English <kenglish_hi@yahoo.com>
2008-09-17 19:37:52 -1000

    Files for exercise 1

---
+++ hw1/exercise_2/question_1/lgsearch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+
+int main(int argc,char *argv[])
+{
+
+    if (argc < 3) {
+	printf("Usage: lgsearch.exe <trials> <step size>\n");
+	return -1;
+    }
+   
+}
