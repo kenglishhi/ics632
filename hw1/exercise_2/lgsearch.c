551e5f65f5c4fa3c45b52784f6416ccc5ab2b152
Kevin English <kenglish_hi@yahoo.com>
2008-09-23 22:27:57 -1000

    new files

--- hw1/exercise_2/lgsearch.c
+++ hw1/exercise_2/lgsearch.c
@@ -14,39 +14,40 @@ int lgsearch(double step_size ) {
 
 
     for (i=0; i < ARRAY_SIZE ; i++ ) {
-	x[i]  = (double) (50.0*rand()/(RAND_MAX+1.0));
-//        x[i]  = 49.8;
-//        printf( "Random number[%d] %1.5f \n", i, x[i] );
+	x[i]  = 49.9  ;//  (double) (50.0*rand()/(RAND_MAX+1.0));
+        printf("x[i] = %f\n", x[i]) ; 
     }
-//    printf("First Result => %10.5f\n", mystery_function_800(&x[0]) ) ;
+
+    double *item;
+    *item = x[0]; 
+    printf("item  = %f\n", *item ) ; 
+    
     gettimeofday(&start,NULL);
 
     double prev_result = 0.0;
     double cur_result = 0.0;
     i = 0;
     int reset = 1;
+    double threshold = 50.0; 
     do {
 	if (reset) {
 	    prev_result = mystery_function_800(&x[0]);
-	    reset= 0;
+	    reset = 0;
 	}
+        printf( " i=%d-- old value = %f, %f, %d \n", i, *item, (*item + step_size),  ((*item + step_size) < threshold ) ) ; 
 
-//      printf(" too big? %d\n", ((x[i] + step_size -50.0) > 0.0) )  ;
-
-	int var = (int) (x[i] + step_size)  ;
-//      printf( " var  = %d (%2.5f + %2.5f) \n", var, x[i], step_size ) ;
-	if (var >= 50)  {
-	    i++;
+        if (! ((*item + step_size) < 50.0 )) { 
+            printf(" too big!")  ;
+	    item +=  1 ;
+            printf(" new item, %f !", *item )  ;
+            i++;
 	    continue ;
-	}
-	x[i] +=  step_size;
-//      printf( " value = %.5f -- cur_result %.5f > %.5f %d \n", x[i], cur_result, prev_result, (cur_result > prev_result)  );
+        } 
+	*item +=  step_size;
+        printf( " i=%d-- new value = %.5f \n", i, *item) ; 
 	cur_result = mystery_function_800(&x[0]);
-//      printf( "arr[%d] %.7f prev =  %5.7f , cur = %5.5f \n",i,  x[i], prev_result, cur_result);
-//      printf( "-- cur_result %.5f > %.5f %d \n", cur_result, prev_result, (cur_result > prev_result)  );
 
 	if ((cur_result - prev_result) > 0.0 ) {
-//       printf( "DOING STEP  %.7f > %.7f \n", cur_result,prev_result  );
 	    prev_result = cur_result;
 	}  else {
 	    x[i] -= step_size;
@@ -55,7 +56,8 @@ int lgsearch(double step_size ) {
 	    printf(".");
 	    fflush(stdout);
 	}
-    } while (i < ARRAY_SIZE) ;
+    } while (i < 10) ;
+//    } while (i < ARRAY_SIZE) ;
     printf("\n");
     gettimeofday(&end,NULL);
     printf("Time Spent %f seconds\n", get_time_diff(&start, &end));
