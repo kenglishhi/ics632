6eb3ffa1ae441e17219dbdc40df45f2685d7f512
Kevin English <kenglish_hi@yahoo.com>
2008-09-26 21:02:55 -1000

    new jacobi series

--- hw1/exercise_3/jacobi.c
+++ hw1/exercise_3/jacobi.c
@@ -4,17 +4,31 @@
 #include <stdlib.h>
 #define N 20 
 
+#### 
+# jacobi iteration formula
+#  n[i,j] = 0.25 * (m[i+1, j] +  m[i-1,j] + m[i,j+1] + m[i,j-1] 
+### 
 
 int main(int argc,char *argv[]) {
-    double v[N][N]; 
-    double b[N][N]; 
+    double output[N][N]; 
+    double m[N][N]; 
     int i, j;
-    for (i=0; i<N; i++){
+
+    for (i=1; i<N; i++){
         for (j=0; j<N; j++){
-            v[i][j] = 0.0;
-            b[i][j] = 0.0;
+            m[i][j] = 0.0;
         }
     }
+    for (i=0; i<N; i++){
+        m[i][0] = 1.0;
+    } 
+
+    for (i=1; i<N; i++){
+        for (j=0; j<N; j++){
+            output[i][j] = 0.0;
+        }
+    }
+    for (i=0; i<N; i++){
 
    printf("Hello World\n");
    return 1;
