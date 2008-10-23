1c5e88063538783078b91320ff8245ecb0e59ae3
kenglish <kenglish_hi@yahoo.com>
2008-10-22 21:10:22 -1000

    Added debugging

--- hw3/q3.c
+++ hw3/q3.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
     MPI_Request send_request ; 
     MPI_Request recv_request ; 
    
-    for (step = 0 ; step < 1; step++) {
+    for (step = 0 ; step < nprocs; step++) {
         Ring_Isend(tempS, row_size * matrix_size, &send_request) ;
 	Ring_Irecv(tempR, row_size * matrix_size, &recv_request) ; 
         slice_matrix_multiply(matrix_a,tempS, result_matrix, matrix_size, row_size,  rank, nprocs, step)  ; 
