f31eb66f053e3daf0b8e6929566d249bf54f71b8
Kevin English <kenglish@chuns.linuxdev.us.dell.com>
2008-11-04 22:13:43 -1000

    new sw_basic.c

--- smith-waterman/sw_basic.c
+++ smith-waterman/sw_basic.c
@@ -25,6 +25,7 @@ void print_matrix(int *matrix, int matrix_size) {
 
 void init_edges(int *matrix, int matrix_size) {
     int i; 
+    srand(time(0)); 
     int my_seed = getpid() ; 
     for (i=0; i< matrix_size; i++ ) { 
         matrix[INDEX(i,0, matrix_size)] = - (1+ (int) (50.0 * (rand() / (RAND_MAX + 1.0))));  
@@ -55,30 +56,28 @@ int get_score(int top_left, int top, int left) {
 
 } 
 
-
-
 int main(int argc, char **argv) {
 
-   int matrix_size = 12 ; 
+    int matrix_size = 12 ; 
 
     struct timeval total_start; 
     struct timeval total_finish; 
-
+  
     gettimeofday(&total_start,NULL); 
 
 //    int row_size = matrix_size / nprocs;
-
 //    double *matrix_a, *matrix_b, *result_matrix, *tempR, *tempS, *tempX;
-    int *matrix_a; // , *matrix_b, *result_matrix, *tempR, *tempS, *tempX;
+
+    int *matrix_a; 
 
     matrix_a = (int *) calloc(matrix_size * (matrix_size), sizeof(int)) ;
-     init_edges(matrix_a, matrix_size );  
+    init_edges(matrix_a, matrix_size );  
     int i,j; 
     for (i=1; i < matrix_size; i++ ) { 
-      for (j=1; j< matrix_size; j++ ) { 
-        matrix_a[INDEX(i,j, matrix_size)] = get_score(matrix_a[INDEX(i-1,j-1, matrix_size)], matrix_a[INDEX(i-1,j, matrix_size)], matrix_a[INDEX(i,j-1, matrix_size)] ) ; 
+        for (j=1; j< matrix_size; j++ ) { 
+            matrix_a[INDEX(i,j, matrix_size)] = get_score(matrix_a[INDEX(i-1,j-1, matrix_size)], matrix_a[INDEX(i-1,j, matrix_size)], matrix_a[INDEX(i,j-1, matrix_size)] ) ; 
 //        printf("matrix_a[%d][%d] = %d \n", i, j, matrix_a[INDEX(i,j, matrix_size)] ); 
-      } 
+        } 
     } 
     print_matrix(matrix_a, matrix_size);     
     return 1; 
